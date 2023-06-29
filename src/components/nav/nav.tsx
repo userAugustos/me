0dd852e1780a424a3c65258ac2321ea3c94cb6ce
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-06-29 13:06:43 -0300

    fix: added the redirect to nav buttons

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -3,17 +3,21 @@ import {ThemeChanger} from "../themeChanger.tsx";
 
 export const Nav = () => {
 
-  // const redirect = () => {
-  //   window.mo
-  // }
+  const redirect = (event:   React.MouseEvent<HTMLElement, MouseEvent>) => {
+    if(event.currentTarget.classList[0] === 'github'){
+      window.open('https://www.github.com/userAugustos')
+      return
+    }
+    window.open('https://www.linkedin.com/in/felipe-augustos/')
+  }
 
   return (
     <nav className="flex relative">
-      <section className="github flex">
+      <section className="github flex" onClick={redirect}>
         <img src="/github.svg" alt="my github"/>
         userAugustos
       </section>
-      <section className="linkedin flex">
+      <section className="linkedin flex" onClick={redirect}>
         <img src="/linkedin.svg" alt="my linkedin"/>
         Felipe Augustos
       </section>
