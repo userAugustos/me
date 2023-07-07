b5f212f7d747c8854eee6fea52034686b839dc44
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-07-07 18:29:01 -0300

    feat: add projectTraker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Nav } from "@components/nav/nav";
 import {Hero} from "@components/hero/hero.tsx";
 import {Wave} from "@components/wave/wave.tsx";
+import {ProjectTracker} from "@components/projectTracker/projectTracker.tsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Hero />
       <Wave />
+      <ProjectTracker />
     </section>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '@styles/index.css'
-import '@styles/themeSwitcher.css'
+import { register } from "swiper/element/bundle";
 import {ThemeProvider} from "./hooks/theme.tsx";
+import '@styles/themeSwitcher.css'
+import 'swiper/css';
+import 'swiper/css/bundle'
+import '@styles/index.css'
+
+register();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
