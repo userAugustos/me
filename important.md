c8cec10e1630080f608344c01310b2359ddb8a4a
Felipe <felipe_augustos84@outlook.com.br>
2026-05-04 16:38:24 -0300

    chore: cleanup

--- important.md
+++
@@ -1,3 +0,0 @@
-https://dribbble.com/shots/10678364-Round-Cat
-
-https://dribbble.com/shots/14508210-Cat-Animation
--- src/components/timeLine/timeLine.tsx
+++
@@ -1,9 +0,0 @@
-export function TimeLine() {
-  return (
-    <div className='timeline'>
-      <section className="line">
-        <i></i>
-      </section>
-    </div>
-  )
-}
