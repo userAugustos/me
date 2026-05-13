1be2a5f110ab6ccdb7163ecfe549948bd4a90d2d
Felipe <felipe_augustos84@outlook.com.br>
2026-05-12 22:14:03 -0300

    fix: removed base path now that we have domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.BASE_PATH ?? '/',
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  // base: process.env.BASE_PATH ?? '/',
+  plugins: [tailwindcss()],
+});
